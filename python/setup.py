1a7f7c261fe327021169e1ec7e6a954176b87f40
kcao <kcao@libnet.sh.cn>
2016-02-05 12:20:05 +0800

    Initial Commit

--- python/setup.py
+++ python/setup.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-module_name = 'xunleidata'
+module_name = 'xltasks'
 module_version = '0.0.1'
 
 setup(name=module_name,
       version=module_version,
-      py_modules=['xunleidata'],
+      py_modules=['xltasks'],
       requires=['sqlobject'])
--- python/test.py
+++ python/test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from xunleidata import XLTaskDb
+from xltasks import XLTaskDb
 xunlei = XLTaskDb()
 xunlei.task('d:\\Tools\\', 'http://www.hyqb.sh.cn/', Name='index.html')
